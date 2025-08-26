4b0efc2bf4606b39cc1521fe9a55f40149bb50e6
kana.kita <kitagawa@ham.works>
2025-08-26 10:35:49 +0900

    main.tsxのeslintエラー修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-createRoot(document.querySelector("#content")).render(<App />);
+const container = document.querySelector("#content");
+const root = createRoot(container as Element);
+root.render(<App />);
